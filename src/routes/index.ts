7776106a7828696516c470f716f84d868313f3d7
Matheus <matheussbp2014@gmail.com>
2023-02-08 22:28:34 -0300

    Desenvolvendo o controller da pesquisa

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 
 import * as PageController from '../controllers/pageController'
+import * as SearchController from '../controllers/searchController'
 
 const router = Router()
 
@@ -8,5 +9,6 @@ router.get('/', PageController.home)
 router.get('/dogs', PageController.dog)
 router.get('/cats', PageController.cat)
 router.get('/fishes', PageController.fish)
+router.get('/search', SearchController.search)
 
 export default router
