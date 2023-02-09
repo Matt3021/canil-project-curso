2d2d73cb502cba3fdc8c1cf5a022f98106feea1a
Matheus <matheussbp2014@gmail.com>
2023-02-08 22:11:42 -0300

    Desenvolvendo a funcionalidade do menu e terminando o controller das páginas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,5 +5,8 @@ import * as PageController from '../controllers/pageController'
 const router = Router()
 
 router.get('/', PageController.home)
+router.get('/dogs', PageController.dog)
+router.get('/cats', PageController.cat)
+router.get('/fishes', PageController.fish)
 
 export default router
